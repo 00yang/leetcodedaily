0d5971eb9f8d2a9e446e9c823a20f9a0dbac9f45
10547 <hyycl39@163.com>
2020-07-31 01:22:11 +0800

    注释了两行

--- jdbc2020720/src/com/company/Main.java
+++ jdbc2020720/src/com/company/Main.java
@@ -3,31 +3,31 @@ import java.sql.*;
 
 public class Main {
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-	// write your code here
-//        System.out.println("hello world");
-//        Class.forName("com.mysql.cj.jdbc.Driver");
+//    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+//	// write your code here
+////        System.out.println("hello world");
+////        Class.forName("com.mysql.cj.jdbc.Driver");
+////
+////        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test?useSSL=false&serverTimezone=UTC","root","123456");
+////
+////        //定义sql语句
+////        String sql1 = "insert into userinfo(name,password) values ('lalala','123')";
+////
+////        //创建执行sql的对象
+////        Statement sm = conn.createStatement();
+////
+////        int count = sm.executeUpdate(sql1);
+////
+////        System.out.println(count);
+////
+////        sm.close();
+////        conn.close();
 //
-//        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test?useSSL=false&serverTimezone=UTC","root","123456");
 //
-//        //定义sql语句
-//        String sql1 = "insert into userinfo(name,password) values ('lalala','123')";
 //
-//        //创建执行sql的对象
-//        Statement sm = conn.createStatement();
 //
-//        int count = sm.executeUpdate(sql1);
 //
-//        System.out.println(count);
-//
-//        sm.close();
-//        conn.close();
-
-
-
-
-
-    }
+//    }
 
 
 
